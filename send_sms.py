5926c2f27dc502ec416e63de24ac59d7f7852b19
Curovearth <mstrider10@gmail.com>
2022-06-20 16:02:38 +0530

    add twilio sms .py [Just for reference]

---
+++ send_sms.py
@@ -0,0 +1,18 @@
+import os
+from twilio.rest import Client
+
+
+# Find your Account SID and Auth Token at twilio.com/console
+# and set the environment variables. See http://twil.io/secure
+account_sid = os.environ['TWILIO_ACCOUNT_SID']
+auth_token = os.environ['TWILIO_AUTH_TOKEN']
+client = Client(account_sid, auth_token)
+
+message = client.messages \
+                .create(
+                     body="Join Earth's mightiest heroes. Like Kevin Bacon.",
+                     from_='+18106411114',
+                     to='+91 8287047870'
+                 )
+
+print(message.sid)
